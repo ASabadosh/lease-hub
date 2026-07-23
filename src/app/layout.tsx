04aebc75b85ea52e06151468aee0ffc900657de0
Avi Sabadosh <asabadosh895@gmail.com>
2026-07-23 14:46:35 -0700

    feat: navbar + titlebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "../components/navbar";
+import TitleBar from "../components/titlebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +30,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {children}
+        <TitleBar />
+        <div className="flex">
+            <Navbar />
+            {children}
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div className="flex h-20"> 
-        <h1>
-          LeaseHub
-        </h1>
-      </div>
-      <div className="flex">
-      <div className="flex flex-col items-center w-64">
-        <button className="p-5 h-10 items-center border-blue-600 bg-white px-4 text-sm font-medium text-blue-600"> 
-          + Upload lease
-        </button>
-        <button className="flex pt-5 text-sm font-medium text-blue-600">
-          All Leases
-        </button>
-      </div>
-
-      </div>
+      
     </main>
   );
 }
