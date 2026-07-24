d08d5acd3faafe9c8dabf4118257c47d16421ba0
Avi Sabadosh <asabadosh895@gmail.com>
2026-07-24 16:36:06 -0700

    feat: initial UI sturcture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      
+      <div className="flex">
+      <div className="flex flex-col w-80 p-5">
+        <h2 className="text-xl font-semibold text-gray-900">
+            All Leases
+        </h2>
+      </div>
+      </div>
     </main>
   );
 }
---
+++ src/app/upload-lease/page.tsx
@@ -0,0 +1,7 @@
+export default function LeaseDisplay() {
+    return (
+        <main>
+            
+        </main>
+    );
+}
