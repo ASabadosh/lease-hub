c82c069d958744b3e9917dae294b241bce8c50b7
Avi Sabadosh <asabadosh895@gmail.com>
2026-07-27 10:50:08 -0700

    refactor: moves tailwind from leasePreview to encapsulating button

--- src/components/leasePreview.tsx
+++ src/components/leasePreview.tsx
@@ -2,8 +2,8 @@ import { Lease } from "@/models/Lease";
 
 export default function LeasePreview(props: Lease) {
     return (
-        <div className = "cursor-pointer rounded-sm border border-gray-200 bg-white p-4 transition-colors hover:bg-gray-50">
-            <h3 className="text-sm font-semibold text-gray-900">
+        <div>
+            <h3 className="text-left text-sm font-semibold text-gray-900">
             {props.title}
             </h3>
         </div>
