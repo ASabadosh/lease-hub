e2c90870490254d1b38c4b6d7f9c5257bebe84f8
Avi Sabadosh <asabadosh895@gmail.com>
2026-08-03 12:15:05 -0700

    feat: add row button. preserved added rows between renders. confirmation button.

--- src/components/leaseTable.tsx
+++ src/components/leaseTable.tsx
@@ -1,53 +1,69 @@
+"use client";
 import "react-tabulator/lib/css/tabulator.min.css";
-import 'react-tabulator/lib/styles.css';
-import { ReactTabulator } from 'react-tabulator';
-import { Lease } from '@/models/Lease'; 
-import { LeaseFieldObject } from '@/models/LeaseFieldObject';
-import { AdditionalLeaseFieldObject } from '@/models/AdditionalLeaseFieldObject';
-import styles from './leaseTable.module.css';
+import "react-tabulator/lib/styles.css";
+import { ReactTabulator } from "react-tabulator";
+import { Lease } from "@/models/Lease";
+import { TableRow } from "@/models/TableRow";
+import { AdditionalLeaseField } from "@/models/AdditionalLeaseField";
+import styles from "./leaseTable.module.css";
 import { addAdditionalLeaseField } from "@/database/addAdditionalLeaseField";
-
+import { updateConfirmation } from "@/database/updateConfirmation";
 
 type LeaseTableProps = {
-    lease: Lease;
-    additional_lease_fields: AdditionalLeaseFieldObject[];
-    };
+  lease: Lease;
+  additional_lease_fields: AdditionalLeaseField[];
+  onFieldAdded: (newField: AdditionalLeaseField) => void;
+};
 
 const columns = [
-    { 
-        title: "Key Information", 
-        field: "field",
-        formatter: "textarea", 
-        widthGrow: 1,
-        variableHeight: true,
-        headerSort: false,
+  {
+    title: "Key Information",
+    field: "field",
+    formatter: "textarea",
+    widthGrow: 1,
+    variableHeight: true,
+    headerSort: false,
+  },
+  {
+    title: "Value",
+    field: "value",
+    formatter: "textarea",
+    widthGrow: 3,
+    variableHeight: true,
+    headerSort: false,
+  },
+  {
+    title: "Confirmed",
+    field: "confirmed",
+    formatter: "tickCross",
+    editor: "tickCross",
+    widthGrow: 1,
+    headerSort: false,
+    cellEdited: function (cell: any) {
+      updateConfirmation(cell);
     },
-    { 
-        title: "Value", 
-        field: "value", 
-        formatter: "textarea", 
-        widthGrow: 3, 
-        variableHeight: true,
-        headerSort: false,
-    },
-    { 
-        title: "Confirmed", 
-        field: "confirmed",
-        formatter: "tickCross",
-        widthGrow: 1,
-        headerSort: false,
-    },
-    { 
-        title: "Clauses", 
-        field: "clauses",
-        formatter: "textarea", 
-        widthGrow: 1, 
-        variableHeight: true,
-        headerSort: false,
-    },
+  },
+  {
+    title: "Clauses",
+    field: "clauses",
+    formatter: "textarea",
+    widthGrow: 1,
+    variableHeight: true,
+    headerSort: false,
+  },
+  {
+    title: "ID",
+    field: "id",
+    visible: false,
+  },
+  {
+    title: "Source",
+    field: "sourceTable",
+    visible: false,
+  },
 ];
 
-function isLeaseFieldObject(value: unknown): value is LeaseFieldObject {
+function isLeaseFieldObject(value: unknown): value is TableRow {
   return (
     typeof value === "object" &&
     value !== null &&
@@ -58,56 +74,78 @@ function isLeaseFieldObject(value: unknown): value is LeaseFieldObject {
   );
 }
 
-async function handleClick(lease: Lease) {
-    addAdditionalLeaseField(lease);
-}
-
-export default function LeaseTable({lease, additional_lease_fields}: LeaseTableProps) {
-    let data: LeaseFieldObject[] = [];
-    for (const [key, value] of Object.entries(lease)) {
-        if (key === "id" || key === "title") {
-            continue; // Skip the id and title fields
-        }
-        if (isLeaseFieldObject(value)) {
-            data.push(value);
-        } else {
-            data.push({
-                field: key, 
-                value: "Field not found", 
-                confirmed: false, 
-                clauses: ""
-            })
-        }
+export default function LeaseTable({
+  lease,
+  additional_lease_fields,
+  onFieldAdded,
+}: LeaseTableProps) {
+  const data: TableRow[] = [];
+  for (const [key, value] of Object.entries(lease)) {
+    if (key === "id" || key === "title") {
+      continue; // Skip the id and title fields
     }
-    if (additional_lease_fields.length !== 0) {
-        for (const [key, value] of Object.entries(additional_lease_fields)) {
-        data.push({
-            field: value.field,
-            value: value.value,
-            confirmed: value.confirmed,
-            clauses: value.clauses
-        });
-        }
+    if (isLeaseFieldObject(value)) {
+      data.push({
+        field: value.field,
+        value: value.value,
+        confirmed: value.confirmed,
+        clauses: value.clauses,
+        id: lease.id,
+        sourceTable: "leases",
+      });
+    } else {
+      data.push({
+        field: key,
+        value: "Field not found",
+        confirmed: false,
+        clauses: "",
+        id: lease.id,
+        sourceTable: "leases",
+      });
     }
+  }
+  if (additional_lease_fields.length !== 0) {
+    for (const [key, value] of Object.entries(additional_lease_fields)) {
+      data.push({
+        field: value.field,
+        value: value.value,
+        confirmed: value.confirmed,
+        clauses: value.clauses,
+        id: value.id,
+        sourceTable: "additional_lease_fields",
+      });
+    }
+  }
 
-return (
+  async function handleAddRowClick(lease: Lease) {
+    const new_field = await addAdditionalLeaseField(lease);
+
+    if (new_field != null) {
+      onFieldAdded(new_field);
+    }
+  }
+
+  return (
     <div className="flex flex-col p-5 bg-white">
-        <h1 className="text-2xl font-semibold tracking-tight text-gray-900 mb-5">
-            {lease.title}
-        </h1>
-        <div className={styles.wrapper}>
-            <ReactTabulator
-                data={data}
-                columns={columns}
-                options={{
-                    layout: "fitColumns",
-                    height: "470px",
-                }}
-            />
-        </div>
-        <button onClick={() => handleClick(lease)} className="h-10 w-full border border-gray-200 rounded-b-md text-sm font-medium text-blue-600 cursor-pointer transition-colors hover:bg-blue-50">
+      <h1 className="text-2xl font-semibold tracking-tight text-gray-900 mb-5">
+        {lease.title}
+      </h1>
+      <div className={styles.wrapper}>
+        <ReactTabulator
+          data={data}
+          columns={columns}
+          options={{
+            layout: "fitColumns",
+            height: "470px",
+          }}
+        />
+      </div>
+      <button
+        onClick={() => handleAddRowClick(lease)}
+        className="h-10 w-full border border-gray-200 rounded-b-md text-sm font-medium text-blue-600 cursor-pointer transition-colors hover:bg-blue-50"
+      >
         + Add Row
-        </button>
+      </button>
     </div>
-);
+  );
 }
