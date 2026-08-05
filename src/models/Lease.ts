ea6392bc3937c54297b102ef30a98a28d6edfaca
Avi Sabadosh <asabadosh895@gmail.com>
2026-08-05 12:29:06 -0700

    refactor: fields can no longer be NULL

--- src/models/Lease.ts
+++ src/models/Lease.ts
@@ -1,20 +1,20 @@
-import type { LeaseFieldObject } from "./TableRow";
+import type { LeaseFieldObject } from "./LeaseFieldObject";
 
 //represents row from leases table in database
 export type Lease = {
   id: number;
   title: string;
-  "Premises": LeaseFieldObject | null;
-  "Term": LeaseFieldObject | null;
-  "Rent": LeaseFieldObject | null;
-  "Security Deposit": LeaseFieldObject | null;
-  "Maintenance and Repairs": LeaseFieldObject | null;
-  "Utilities": LeaseFieldObject | null;
-  "Insurance": LeaseFieldObject | null;
-  "Taxes": LeaseFieldObject | null;
-  "Surrender": LeaseFieldObject | null;
-  "Holding Over": LeaseFieldObject | null;
-  "Option to Extend Lease": LeaseFieldObject | null;
-  "Notices": LeaseFieldObject | null;
-  "Attorney Fees": LeaseFieldObject | null;
+  "Premises": LeaseFieldObject;
+  "Term": LeaseFieldObject;
+  "Rent": LeaseFieldObject;
+  "Security Deposit": LeaseFieldObject;
+  "Maintenance and Repairs": LeaseFieldObject;
+  "Utilities": LeaseFieldObject;
+  "Insurance": LeaseFieldObject;
+  "Taxes": LeaseFieldObject;
+  "Surrender": LeaseFieldObject;
+  "Holding Over": LeaseFieldObject;
+  "Option to Extend Lease": LeaseFieldObject;
+  "Notices": LeaseFieldObject;
+  "Attorney Fees": LeaseFieldObject;
 };
