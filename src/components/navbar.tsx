a9219f0902659e6c3e36009743b58f23d291bcb8
Avi Sabadosh <asabadosh895@gmail.com>
2026-08-05 16:46:28 -0700

    feat: added links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export default function Navbar() {
     return (
     <div className="flex flex-col items-center w-50 h-[630px] p-5">
-        <button className="flex p-5 h-10 items-center justify-center w-full border border-blue-600 bg-slate-50 px-4 text-sm font-medium text-blue-600 rounded-md cursor-pointer hover:bg-blue-50 "> 
+        <Link href="/upload-lease" className="flex p-5 h-10 items-center justify-center w-full border border-blue-600 bg-slate-50 px-4 text-sm font-medium text-blue-600 rounded-md cursor-pointer hover:bg-blue-50 "> 
           + Upload Lease
-        </button>
-        <button className="flex mt-5 pl-5 text-sm font-medium text-blue-600 align-items-left w-full cursor-pointer">
+        </Link>
+        <Link href="/" className="flex mt-5 pl-5 text-sm font-medium text-blue-600 align-items-left w-full cursor-pointer">
           All Leases
-        </button>
+        </Link>
       </div>
     );
 }
